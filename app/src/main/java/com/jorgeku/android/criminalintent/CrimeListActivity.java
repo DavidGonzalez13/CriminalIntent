825318692e99355f5751cad1c9232ae5d041181c
Jorge David Ku Gonzalez <14470427@itcampeche.edu.mx>
2017-11-30 09:35:23 -0600

    Version Final Criminalintent

---
+++ app/src/main/java/com/jorgeku/android/criminalintent/CrimeListActivity.java
@@ -0,0 +1,13 @@
+package com.jorgeku.android.criminalintent;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by david on 30/11/2017.
+ */
+
+public class CrimeListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
---
+++ app/src/main/java/com/jorgeku/android/criminalintent/SingleFragmentActivity.java
@@ -0,0 +1,33 @@
+package com.jorgeku.android.criminalintent;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+/**
+ * Created by david on 29/11/2017.
+ */
+
+public abstract  class SingleFragmentActivity extends FragmentActivity {
+
+    protected abstract android.support.v4.app.Fragment createFragment();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+        android.support.v4.app.Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
+
+
